491e3b4efa6ebeada917392ff46789c4997d8dbf
Worldswayfarer <133384338+Worldswayfarer@users.noreply.github.com>
2023-12-07 19:46:49 +0100

    Floor Prototype
    Made a floor "texture" and walls

--- Dungeon/Entity/Enemy/enemy_controller.gd
+++ Dungeon/Entity/Enemy/enemy_controller.gd
@@ -4,10 +4,11 @@ extends Node2D
 @export var _enemy_scene : PackedScene 
 
 var Screen_size = Vector2.ZERO
-
+var _offset = 20
 
 func _ready():
 	Screen_size = get_viewport_rect().size
+	print(Screen_size)
 	randomize()
 	spawnEnemies()
 
@@ -15,6 +16,7 @@ func _ready():
 func spawnEnemies(count = 10):
 	for number in range(count):
 		var new_enemy = _enemy_scene.instantiate()
-		var new_position = Vector2(randi_range(50, Screen_size.x - 50), randi_range(50, Screen_size.y - 50))
+		var new_position = Vector2(randi_range(Screen_size.x + _offset, Screen_size.x - _offset) - Screen_size.x,
+				 randi_range(Screen_size.y + _offset, Screen_size.y - _offset) - Screen_size.y)
 		new_enemy.position = new_position
 		add_child(new_enemy)
