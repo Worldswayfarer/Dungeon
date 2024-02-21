e4bee47956b47dd4fbd3beee3c15707c1cfa0c42
Worldswayfarer <133384338+Worldswayfarer@users.noreply.github.com>
2024-02-21 20:18:26 +0100

    Effect aggregation
    if a skill or subskill has a main_skill (aka an actual usable skill) the on_hit_effects of the upgrades are forwarded to this skill.
    if a main_skill is not usable (aka it is not the root skill) it will only get the effects in the skill and not the upgrades.
    
    self_effects are all aggregated into one list and then applied to the caster

--- Dungeon/SkillTrees/skill.gd
+++ Dungeon/SkillTrees/skill.gd
@@ -5,7 +5,9 @@ var _name
 var _current_level = 0
 var _maximum_level = 3
 
-var _main_skill
+
+var _is_castable : bool
+var _main_skill = null
 
 
 # 2-dimensional array, every level is seperate
@@ -16,11 +18,12 @@ var _on_hit_effects = []
 var _upgrades = []
 
 
-func _init(name : Enums.Abilities, current_level : int, maximum_level : int,
- main_skill, self_effects = [], on_hit_effects = [], upgrades = []):
+func _init(name : Enums.Abilities, current_level : int, maximum_level : int, is_castable : bool,
+ main_skill = null, self_effects = [], on_hit_effects = [], upgrades = []):
 	_name = name
 	_current_level = current_level
 	_maximum_level = maximum_level
+	_is_castable = is_castable
 	_main_skill = main_skill
 	_self_effects = self_effects
 	_on_hit_effects = on_hit_effects
@@ -32,15 +35,43 @@ func level_up():
 		_current_level += 1
 
 
-func use_skill(caster):
-	var combined_effects = _on_hit_effects[_current_level]
-	for upgrade in _upgrades:
-		pass
-	
+func get_on_hit_effects():
+	return _on_hit_effects[_current_level]
+
+
+func get_self_effects():
+	return _self_effects[_current_level]
+
+
+func aggregate_on_hit_effects(caster):
 	var scaled_effects = []
-	for effect in combined_effects:
+	for effect in _on_hit_effects[_current_level]:
 		var new_effect = effect.duplicate()
 		new_effect.scale(caster)
 		scaled_effects.append(new_effect)
-	_main_skill._effects.append_array(scaled_effects)
-	_main_skill.apply_effect(caster)
+	
+	for upgrade in _upgrades:
+		scaled_effects.append(upgrade.aggregate_on_hit_effects(caster))
+	if _main_skill == null:
+		return scaled_effects
+	if _is_castable:
+		_main_skill._effects.append(scaled_effects)
+
+
+func aggregate_self_effects(caster):
+	var scaled_effects = []
+	for effect in _self_effects[_current_level]:
+		var new_effect = effect.duplicate()
+		new_effect.scale(caster)
+		scaled_effects.append(new_effect)
+	
+	for upgrade in _upgrades:
+		scaled_effects.append(upgrade.aggregate_self_effects(caster))
+	
+	return scaled_effects
+
+
+func use_skill(caster):
+	_main_skill.apply_effect(aggregate_on_hit_effects(caster))
+	caster.add_effects(aggregate_self_effects(caster))
+
