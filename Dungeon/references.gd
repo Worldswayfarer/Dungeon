b6b253b6e4b536afca1c76aa4c121fffbce3e7c2
Worldswayfarer <133384338+Worldswayfarer@users.noreply.github.com>
2024-02-28 17:42:21 +0100

    Update references.gd

--- Dungeon/references.gd
+++ Dungeon/references.gd
@@ -1,8 +1,10 @@
 extends Node
 
 @warning_ignore("unused_private_class_variable")
-
 @onready var _factory = get_node("/root/main/ObjectFactory")
+
 #Components
+@warning_ignore("unused_private_class_variable")
 var _health_component = "HealthComponent"
+@warning_ignore("unused_private_class_variable")
 var _ability_component = "AbilityComponent"
