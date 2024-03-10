3b0784f8a9a98103a625c407bc5f7d466531006b
Worldswayfarer <133384338+Worldswayfarer@users.noreply.github.com>
2024-03-10 18:23:25 +0100

    Update Ability.gd

--- Dungeon/SkillTrees/Ability.gd
+++ Dungeon/SkillTrees/Ability.gd
@@ -1,3 +1,14 @@
 extends Node
 
 class_name Ability
+
+
+
+
+
+func use(position, _effects):
+	var factory = References._factory
+	var new_bullet = factory.get_object_instance(Enums.ObjectTypes.BULLET)
+	factory.add_child(new_bullet)
+	
+	new_bullet.setup(position, _effects)
