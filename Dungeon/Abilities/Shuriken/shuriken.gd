ace2a080886f581dac8ae96dc1ddfa3395098a56
Worldswayfarer <133384338+Worldswayfarer@users.noreply.github.com>
2025-07-08 22:12:49 +0200

    Improved shuriken ability (now returns to player), abilities now scale correctly with base stats and caster

--- Dungeon/Abilities/Shuriken/shuriken.gd
+++ Dungeon/Abilities/Shuriken/shuriken.gd
@@ -7,7 +7,10 @@ var _direction : Vector2
 var _duration = 2.
 var _effects : Array = []
 var _hit_box : HitBoxComponent
-var _movement_component : MovementComponent
+var _Movement : MovementComponent
+
+var _isReturning = false
+var _spinning_speed = 15
 
 func get_component_type() -> Enums.ComponentTypes:
 	return Enums.ComponentTypes.LOGIC
@@ -19,18 +22,35 @@ func setup(new_position, target_position, effects : Array = [], layer = 0b100):
 	_hit_box = get_component(Enums.ComponentTypes.HITBOX)
 	_hit_box.collision_layer = layer
 
-	_movement_component = get_component(Enums.ComponentTypes.MOVEMENT)
+	_Movement = get_component(Enums.ComponentTypes.MOVEMENT)
 	_parent.position = new_position
 	_direction = target_position - _parent.position
 	_parent.rotation = _direction.angle()
-	_movement_component.change_direction(_direction.normalized())
+	_Movement.change_direction(_direction.normalized())
+
+	
 
 
 func _process(delta):
-	if _duration <= 0:
-		_movement_component.change_direction(-_direction.normalized())
+	if _duration <= 0 and !_isReturning:
+		handle_returning()
 	_duration -= delta
+	_parent.rotate(_spinning_speed * delta)
 
 
 func handle_hitbox_entered(area):
+	if area.get_type() == Enums.ObjectTypes.PLAYER:
+		_parent.queue_free()
+		return
 	area.add_effects(_effects)
+
+
+func handle_returning():
+	_isReturning = true
+	_hit_box.collision_mask = 0b110
+	Signals.player_position_updated.connect(change_direction)
+
+
+func change_direction(player_position):
+	var direction = player_position - _parent.position
+	_Movement.change_direction(direction.normalized())
--- Dungeon/SkillTrees/Ability.gd
+++ Dungeon/SkillTrees/Ability.gd
@@ -1,11 +1,15 @@
 class_name Ability
-
 var _effects = []
-
 
 func use(caster, projectile):
 	var factory = References._factory
 	var new_projectile = factory.get_object_instance(projectile)
 	factory.add_child(new_projectile)
 	var logic = new_projectile.get_component(Enums.ComponentTypes.LOGIC)
-	logic.setup(caster.position, caster.get_viewport().get_mouse_position(), _effects)
+
+	var damage_effect = DamageEffect.new(1)
+	damage_effect.scale(new_projectile)
+	damage_effect.scale(caster)
+	var effects = [damage_effect]
+
+	logic.setup(caster.position, caster.get_viewport().get_mouse_position(), effects)
